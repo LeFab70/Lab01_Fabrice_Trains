f0fbdf6817841f302011c10734ab2fd1da6858e1
LeFab70 <kouonang2002@gmail.com>
2025-07-14 21:24:04 -0300

    Initial Commit

---
+++ Lab01_Fabrice_Trains/Lab01_Fabrice_TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_Fabrice_TrainsApp.swift
+//  Lab01_Fabrice_Trains
+//
+//  Created by Fabrice Kouonang on 2025-07-14.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_Fabrice_TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
